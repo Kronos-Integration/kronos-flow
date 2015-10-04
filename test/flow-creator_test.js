457be6155fd01c5deef79721ffbdaf6f21cf1c0d
Torsten Link <torsten.link@db.com>
2015-10-04 16:16:48 +0200

    working on flow

--- test/flow-creator_test.js
+++ test/flow-creator_test.js
@@ -1,4 +1,4 @@
-/*global describe, it*/
+/* global describe, it, beforeEach */
 /* jslint node: true, esnext: true */
 "use strict";
 
@@ -7,14 +7,50 @@ const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should();
 
+
+const fs = require('fs');
+const path = require("path");
+
+const fixturesDir = path.join(__dirname, 'fixtures');
+
+
 const flowCreatorFactory = require('../lib/flow-creator');
 const Endpoint = require('kronos-step').endpoint;
+const dumyStepFactory = require('kronos-step-passthrough');
+const messageFactory = require('kronos-step').message;
 
-// flow without description
-// flow without inbound step
 
-describe('flow-creator', function () {
+let kronos;
+let defaultMessage;
 
+describe('flow-creator: Extended functionality', function () {
+
+	beforeEach(function () {
+		// Prepare the the dummy steps needed by the flow
+
+		kronos = {};
+		kronos.steps = {};
+		kronos.steps.Step_1 = dumyStepFactory;
+		kronos.steps.Step_2 = dumyStepFactory;
+		kronos.steps.Step_3 = dumyStepFactory;
+		kronos.steps.Step_4 = dumyStepFactory;
+		kronos.steps.Step_5 = dumyStepFactory;
+
+		defaultMessage = messageFactory({
+			"my": "name"
+		});
+
+	});
+
+
+	it('Flow with endpoints', function (done) {
+		testObject(kronos, 'flow_with_endpoints.json', defaultMessage, done);
+	});
+
+
+});
+
+describe('flow-creator: Base functionality', function () {
 	it('flow: Create a flow with endpoints and connected steps', function (done) {
 
 		flowCreatorFactory({
@@ -205,6 +241,56 @@ describe('flow-creator', function () {
 
 });
 
+/**
+ * This function creates a flow obect out of the given json definition.
+ * Then it will send a message through the flow and checks that the message
+ * will arrive at the out endpoint
+ * @param kronos The kronos object which holds the step definitions
+ * @param flowDefinitionJsonFile The filename of the json file to load. Relative to the fixtures directory.
+ * @param sourceMessage The message to be send through the flow
+ * @param done The 'done' callback
+ */
+function testObject(kronos, flowDefinitionJsonFile, sourceMessage, done) {
+	let flowDefintion = require(path.join(fixturesDir, flowDefinitionJsonFile));
+
+
+	// Create the flow objects and get the first one
+	let flow = flowCreatorFactory(kronos, flowDefintion)[0];
+
+
+	// get the Endpoints
+	let inEndPoint = flow.getEndpoint('inFile');
+	let outEndPoint = flow.getEndpoint('outFile');
+
+
+	// This generator emulates the IN endpoint of the next step.
+	// It will be connected with the OUT endpoint of the Adpater
+	let generatorFunction = function* () {
+		while (true) {
+			const message = yield;
+
+
+			// the received message should equal the sended one
+			// before comparing delete the hops
+			message.hops = [];
+
+			assert.deepEqual(message, sourceMessage);
+			done();
+		}
+	};
+
+	outEndPoint.connectedEndpoint = generatorFunction;
+	outEndPoint.outActiveIterator = generatorFunction();
+	outEndPoint
+		.outActiveIterator
+		.next();
+
+	let it = inEndPoint.getInPassiveIterator()();
+	it.next();
+
+	it.next(sourceMessage);
+
+}
 
 
 function mockStepFactory(kronos, flow, opts) {
---
+++ test/gumbo.js
@@ -0,0 +1,93 @@
+/* global describe, it, beforeEach */
+/* jslint node: true, esnext: true */
+"use strict";
+
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+
+
+const fs = require('fs');
+const path = require("path");
+
+const fixturesDir = path.join(__dirname, 'fixtures');
+
+
+const flowCreatorFactory = require('../lib/flow-creator');
+const Endpoint = require('kronos-step').endpoint;
+const dumyStepFactory = require('kronos-step-passthrough');
+const messageFactory = require('kronos-step').message;
+
+
+let kronos;
+let defaultMessage;
+
+
+kronos = {};
+kronos.steps = {};
+kronos.steps.Step_1 = dumyStepFactory;
+kronos.steps.Step_2 = dumyStepFactory;
+kronos.steps.Step_3 = dumyStepFactory;
+kronos.steps.Step_4 = dumyStepFactory;
+kronos.steps.Step_5 = dumyStepFactory;
+
+defaultMessage = messageFactory({
+	"my": "name"
+});
+
+testObject(kronos, 'flow_with_endpoints.json', defaultMessage);
+
+
+
+/**
+ * This function creates a flow obect out of the given json definition.
+ * Then it will send a message through the flow and checks that the message
+ * will arrive at the out endpoint
+ * @param kronos The kronos object which holds the step definitions
+ * @param flowDefinitionJsonFile The filename of the json file to load. Relative to the fixtures directory.
+ * @param sourceMessage The message to be send through the flow
+ * @param done The 'done' callback
+ */
+function testObject(kronos, flowDefinitionJsonFile, sourceMessage) {
+	let flowDefintion = require(path.join(fixturesDir, flowDefinitionJsonFile));
+
+
+	// Create the flow objects and get the first one
+	let flow = flowCreatorFactory(kronos, flowDefintion)[0];
+
+	console.log(flow);
+
+	// get the Endpoints
+	let inEndPoint = flow.getEndpoint('inFile');
+	let outEndPoint = flow.getEndpoint('outFile');
+
+
+	// This generator emulates the IN endpoint of the next step.
+	// It will be connected with the OUT endpoint of the Adpater
+	let generatorFunction = function* () {
+		while (true) {
+			const message = yield;
+
+
+			// the received message should equal the sended one
+			// before comparing delete the hops
+			message.hops = [];
+
+			assert.deepEqual(message, sourceMessage);
+		}
+	};
+
+	outEndPoint.connectedEndpoint = generatorFunction;
+	outEndPoint.outActiveIterator = generatorFunction();
+	outEndPoint
+		.outActiveIterator
+		.next();
+
+
+	let it = inEndPoint.getInPassiveIterator()();
+	it.next();
+
+	it.next(sourceMessage);
+
+}
