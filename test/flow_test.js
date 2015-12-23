17c4959acc516ebc05de3637562b6b2e63c124e1
Markus Felten <Markus.Felten@gmx.de>
2015-12-23 02:04:18 +0100

    test: reenable livecycle test

--- test/flow_test.js
+++ test/flow_test.js
@@ -13,92 +13,90 @@ const chai = require('chai'),
 	step = require('kronos-step');
 
 
-// // ---------------------------
-// // Create a mock manager
-// // ---------------------------
-// const manager = testStep.managerMock;
-//
-// // register the flow
-// flow.registerWithManager(manager);
-//
-// // register the passthroughStep
-// stepPassThrough.registerWithManager(manager);
-//
-// // create 5 copies and register them also
-// for (let i = 1; i < 6; i++) {
-// 	const step = manager.getStepInstance({
-// 		"type": "kronos-step-passthrough",
-// 		"name": "Step_" + i
-// 	});
-// 	manager.registerStepImplementation(step);
-// }
-//
-// /**
-//  * A dummy step for flow testing. The step will execute the stop
-//  * and start command after the given amaount of time
-//  * @param stepName The name of this step
-//  * @param time The time in millisecond it will last until the promise is fullfilled
-//  */
-// const DummyStep = {
-// 	"name": "dummy",
-//
-// 	_initialize(manager, scopeReporter, name, stepConfiguration, endpoints, props) {
-// 		props.time = {
-// 			value: stepConfiguration.time
-// 		};
-// 		console.log(`DummyStep: time: ${stepConfiguration.time}`);
-// 	},
-//
-// 	_start() {
-// 		let self = this;
-// 		return new Promise(function (fulfill, reject) {
-// 			setTimeout(() => {
-// 					console.log(`Started the step '${self.name}'`);
-// 					fulfill(self);
-// 				},
-// 				self.time);
-// 		});
-// 	},
-//
-// 	_stop() {
-// 		let self = this;
-// 		return new Promise(function (fulfill, reject) {
-// 			setTimeout(() => {
-// 				console.log(`Stopped the step '${self.name}'`);
-// 				fulfill(self);
-// 			}, self.time);
-// 		});
-// 	}
-// };
-//
-// manager.registerStepImplementation(DummyStep);
-//
-//
-// describe('livecycle', function () {
-//
-// 	// load the content of the flow definition
-// 	flow.loadFlows(manager, manager.scopeReporter, {
-// 		"myFlowName": {
-// 			"type": "kronos-flow",
-// 			"steps": {
-// 				"slowInbound": {
-// 					"type": "dummy",
-// 					"time": 50
-// 				},
-// 				"normal": {
-// 					"type": "dummy",
-// 					"time": 10
-// 				},
-// 				"slowOutbound": {
-// 					"type": "dummy",
-// 					"time": 70
-// 				}
-// 			}
-// 		}
-// 	});
-//
-// 	const testFlow = manager.getFlow("myFlowName");
-//
-//
-// 	testStep.checkStepLivecycle(manager, testFlow, function (step, state) {});
-// });
+// ---------------------------
+// Create a mock manager
+// ---------------------------
+const manager = testStep.managerMock;
+
+// register the flow
+require('../index').registerWithManager(manager);
+
+/*
+// register the passthroughStep
+stepPassThrough.registerWithManager(manager);
+
+// create 5 copies and register them also
+for (let i = 1; i < 6; i++) {
+	manager.registerStepImplementation(manager.getStepInstance({
+		"type": "kronos-step-passthrough",
+		"name": "Step_" + i
+	}));
+}
+*/
+
+/**
+ * A dummy step for flow testing. The step will execute the stop
+ * and start command after the given amaount of time
+ * @param stepName The name of this step
+ * @param time The time in millisecond it will last until the promise is fullfilled
+ */
+manager.registerStepImplementation(Object.assign({}, step.Step, {
+	"name": "slow-start",
+
+	initialize(manager, scopeReporter, name, stepConfiguration, endpoints, props) {
+		props.time = {
+			value: stepConfiguration.time
+		};
+		//console.log(`DummyStep: time: ${stepConfiguration.time}`);
+	},
+
+	_start() {
+		const self = this;
+		return new Promise(function (fulfill, reject) {
+			setTimeout(() => {
+					//console.log(`Started the step '${self.name}'`);
+					fulfill(self);
+				},
+				self.time);
+		});
+	},
+
+	// TODO why does "can be stopped while starting" not work
+	__stop() {
+		const self = this;
+		return new Promise(function (fulfill, reject) {
+			setTimeout(() => {
+				//console.log(`Stopped the step '${self.name}'`);
+				fulfill(self);
+			}, self.time);
+		});
+	}
+}));
+
+describe('livecycle', function () {
+
+	// load the content of the flow definition
+	flow.loadFlows(manager, manager.scopeReporter, {
+		"myFlowName": {
+			"type": "kronos-flow",
+			"steps": {
+				"slowInbound": {
+					"type": "slow-start",
+					"time": 10
+				},
+				"normal": {
+					"type": "slow-start",
+					"time": 5
+				},
+				"slowOutbound": {
+					"type": "slow-start",
+					"time": 70
+				}
+			}
+		}
+	});
+
+	const testFlow = manager.getFlow("myFlowName");
+
+	testStep.checkStepLivecycle(manager, testFlow);
+});
