6c2501d1ce5c14a738477e73605111b07fcf174e
Markus Felten <Markus.Felten@gmx.de>
2018-07-17 15:00:40 +0200

    chore(package): add rollup-plugin-commonjs@^9.1.3 chore(package): add rollup-plugin-node-resolve@^3.3.0 chore(package): add rollup-plugin-istanbul@^2.0.1 chore(package): add rollup-plugin-executable@^1.2.0 chore(package): semantic-release@^15.8.0 chore(rollup): import resolve,commonjs,executable chore(rollup): import resolve,commonjs,istanbul chore(rollup): add istanbul,resolve,commonjs

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,3 +1,7 @@
+import istanbul from 'rollup-plugin-istanbul';
+
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
@@ -9,7 +13,9 @@ export default {
     'kronos-service',
     'model-attributes'
   ],
-  plugins: [multiEntry()],
+  plugins: [multiEntry(), istanbul({
+    exclude: ['tests/**/*-test.js']
+  }), resolve(), commonjs()],
   output: {
     file: 'build/bundle-test.js',
     format: 'cjs',
