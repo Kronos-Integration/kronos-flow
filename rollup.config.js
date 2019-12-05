e8f5e32f8a3f108a12e4f13c02bc419ef0c10e61
Markus Felten <Markus.Felten@gmx.de>
2019-12-05 16:47:13 +0100

    fix(rollup): use @rollup/plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import json from "rollup-plugin-json";
 import cleanup from 'rollup-plugin-cleanup';
 import executable from 'rollup-plugin-executable';
 import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
 
 export default {
